e1363627988a1307d1907664997717c3abfb6d46
INGRID BATISTA CONCEICAO <ingrid.bconceicao@senacsp.edu.br>
2024-03-19 10:40:34 -0300

    19/03/2024

---
+++ app.js
@@ -0,0 +1,18 @@
+//importando o express
+const express = require('express');
+
+//inicializando o express
+const app = express();
+
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+    res.send("Deu bom :) !")
+})
+
+
+//configuração da porta do servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
