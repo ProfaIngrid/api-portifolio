3099b6a4aa0467d2a232dd31325a8c319400d227
INGRID BATISTA CONCEICAO <ingrid.bconceicao@senacsp.edu.br>
2024-03-20 08:45:30 -0300

    2024-03-20

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-//importando o express
-const express = require('express');
-
-//inicializando o express
-const app = express();
-
+const express = require('express'); //importando o express
+const app = express(); //inicializando o express
 const port = 3000;
-
+const usersRouter = require('./routes/usersRoute');
 
 app.get("/", (req, res) => {
     res.send("Deu bom :) !")
 })
+
+
 
 
 //configuração da porta do servidor
---
+++ controllers/usersController.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dbConecta = require('../models/dbConnection');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    dbConecta.query('SELECT * FROM users', (err, result) => {
+        if(err) throw err;
+        res.json(result);
+    })
+})
+
+module.exports = router;
